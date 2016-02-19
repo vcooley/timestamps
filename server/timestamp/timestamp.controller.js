8a6281cd783e1582c0afec0c0e38d1516c9c6688
Vince <vince.r.cooley@gmail.com>
2016-02-19 14:15:07 -0600

    Test template completed. Minor refactor of controller

--- server/timestamp/timestamp.controller.js
+++ server/timestamp/timestamp.controller.js
@@ -1,5 +1,16 @@
 'use strict';
 
+function getNaturalDate(date) {
+  var options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC'
+  };
+
+  return date.toLocaleString('en-US', options);
+}
+
 var showTime = function (req, res) {
   var timestamp = req.params.timestamp;
   var date;
@@ -18,10 +29,11 @@ var showTime = function (req, res) {
 
   // If the timestamp was valid, set timeObj properties up.
   if (date) {
-    var options = {year: 'numeric', month: 'long', day: 'numeric'};
     timeObj.unix = date.getTime();
-    timeObj.natural = date.toLocaleString('en-US', options);
+    timeObj.natural = getNaturalDate(date);
   }
+
+  // Send response
   res
     .status(200, 'OK')
     .json(timeObj);
--- server/timestamp/timestamp.spec.js
+++ server/timestamp/timestamp.spec.js
@@ -2,6 +2,7 @@
 
 var app = require('../app');
 var request = require('supertest');
+var should = require('should');
 var timestamp = require('./timestamp.controller');
 
 describe('/time', function() {
@@ -11,24 +12,39 @@ describe('/time', function() {
     it('should send JSON with null values on invalid time', function(done) {
       request(app)
         .get('/time/here-is-an-invalid-time')
-        .expect('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
         .end(function(err, res) {
           if (err) {
             throw err;
           }
           res.body.should.have.property('unix');
-          res.body.unix.should.be(null)
+          should(res.body.unix).equal(null);
           res.body.should.have.property('natural');
-          res.body.natural.should.be(null)
+          should(res.body.natural).equal(null);
           done();
         });
     });
     it('should send unix and natural date with UNIX parameter', function(done) {
-
+      var times = [0, -200000, 100, 100000000, 1];
+      times.forEach(function(time) {
+        var date = new Date(time);
+        request(app)
+          .get('/time/' + time)
+          .expect('Content-Type', /json/)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            res.body.should.have.property('unix');
+            res.body.unix.should.equal(date.getTime());
+            res.body.should.have.property('natural');
+            res.body.natural.should.equal();
+            done();
+          });
+      });
     });
-    it('should send unix and natural date with natural parameter', function(done) {
 
-    });
+    it('should send unix and natural date with natural parameter');
 
   });
 
