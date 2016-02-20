67f156ad217429f5d41e0e89e753acb7cf891d19
Vince Cooley <vince.r.cooley@gmail.com>
2016-02-20 15:53:26 -0600

    Heroku deployment changes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 
 // Set default environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PORT = process.env.PORT || 9000;
 
 var express = require('express');
 
@@ -10,9 +11,9 @@ var app = express();
 var server = require('http').createServer(app);
 require('./routes')(app);
 
-app.set('port', (process.env.PORT) || 5000);
+app.set('port', process.env.PORT);
 server.listen(app.get('port'), function() {
-  console.log('Express server listening on %d.', config.port);
+  console.log('Express server listening on %d.', process.env.PORT);
 });
 
 module.exports = app;
